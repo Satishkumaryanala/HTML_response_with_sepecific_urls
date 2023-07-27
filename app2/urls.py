4832102db76b25ebb0db721d20417132593963e5
satishkumaryanala <satishkumaryanala@gmail.com>
2023-07-27 13:34:55 +0530

    first commit

---
+++ app2/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from app2.views import *
+
+app_name='second'
+
+urlpatterns=[
+    path('app2_first/',app2_first,name='app2_first'),
+    path('app2_second/',app2_second,name='app2_second'),
+]
